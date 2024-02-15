88f416aaf84a0ddaa838fa6c9e4122ae31278462
ErzeKawek <130212655+ErzeKawek@users.noreply.github.com>
2024-02-15 17:40:20 +0700

    Inital Commit Of Enditions

---
+++ src/main/java/net/erzekawek/enditions/EnditionsClient.java
@@ -0,0 +1,10 @@
+package net.erzekawek.enditions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnditionsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
